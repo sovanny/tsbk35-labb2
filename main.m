304155d63c31e810832b98ecbd2a56b1c73aaada
Gustav Lagnstrom <glagn@GNONB170.gaia.se>
2019-02-22 17:05:21 +0100

    clean up. ready for test

--- main.m
+++ main.m
@@ -1,43 +1,19 @@
 clear all; close all; clc
-filename = 'hey12';
+filename = 'heyhey';
 [y, fs] = audioread(strcat(filename,'.wav'));
 
-%% 1. Dela in signal i block och transformera
-blocksize = 2^8;
+blocksize = 512;
 n_steps = 24;
 ymin = min(y);
 ymax = max(y);
 
 stepsize = (ymax-ymin)/n_steps;
-y_transformed = mdct(y, blocksize);
+[R,D,SNR] = transform_encoding(y, n_steps, blocksize);
 
-%% 2. Kvantisera transformkomponenterna och räkna ut p
+fprintf('block size:    %i\n', blocksize)
+fprintf('step size:     %f\n', stepsize)
+fprintf('nr of steps:   %i\n', n_steps)
+fprintf('R:             %f\n', R)
+fprintf('D:             %e\n', D)
+fprintf('SNR:           %f\n', SNR)
 
-y_quantized = quantize_equal_simple(y_transformed,stepsize);
-[symbols,~,idx] = unique(y_quantized);
-n = accumarray(idx(:),1);
-p = n/sum(sum(n));
-
-%plot(p)
-
-%% 3. Anv?nd n?gon form av kodning
-R = huffman(p);
-
-
-%% Nytt block för det
-yhat = imdct(y_quantized*stepsize);
-diff = y - yhat;
-D = mean(diff.^2);
-SNR = 10*log10(var(y)/D);
-
-fprintf('=== Results ===\n')
-fprintf('block size:  %i\n', blocksize)
-fprintf('step size:   %f\n', stepsize)
-fprintf('nr of steps: %i\n', n_steps)
-fprintf('R:           %f\n', R)
-fprintf('D:           %f\n', D)
-fprintf('SNR:         %f\n', SNR)
-
-%%
-new_filename = strcat(filename,'_dist.wav');
-audiowrite(new_filename,yhat,fs);
